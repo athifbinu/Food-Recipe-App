45a37685e320f72fddebd812763a553ed4393c2c
Athif binu <binuathif@gmail.com>
2022-12-21 23:33:39 +0530

    add style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Menu from './components/Menu'
+import SpecialDishes from './components/SpecialDishes'
 import "./App.scss"
 function App() {
   return (
     <div>
       <Hero/>
       <Menu/>
+      <SpecialDishes/>
     </div>
   )
 }
---
+++ src/components/SpecialDishes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SpecialDishes() {
+  return (
+    <section className='special-dishes'>
+        <div className="container">
+            <div className="special-dishes-content ">
+            <h2>Our Special Dishes</h2>
+            <br />
+           <p>Lorem ipsum dolor, sit amet consae assumenda quo at libero in quibusdam. Veniam.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default SpecialDishes
